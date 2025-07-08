c3a9aaddc410360d4b85a90c2f6e1ffb3b01373e
Shreyas Inamdar <shreyasinamdar0999@gmail.com>
2025-07-08 17:52:38 +0530

    first commit

---
+++ abc.java
@@ -0,0 +1,42 @@
+import java.util.*;
+interface shape{
+    void area();
+}
+class square implements shape
+{
+int side;
+
+square(int side)
+{
+
+this.side=side;
+
+}
+
+
+public void area()
+{
+
+int area;
+area=side*side;
+System.out.println("area is"+area);
+
+}
+
+
+}
+ class abc {
+    public static void main(String[] args) 
+    {
+        Scanner ob=new Scanner(System.in);
+        System.out.println("enter the side");
+        int s;
+       s=ob.nextInt();
+       square o=new square(s);
+       o.area();
+
+
+
+
+    }
+}
