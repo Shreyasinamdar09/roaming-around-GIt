8155594b8d5cdc8b800116d44f9bd4270022fd09
Shreyas Inamdar <shreyasinamdar0999@gmail.com>
2025-07-09 20:49:43 +0530

    Added custom feature/layer

---
+++ Main.java
@@ -0,0 +1,34 @@
+class a{
+    int x;
+    a(){
+        System.out.println("1");
+    }
+     void main(){
+        System.out.println("1 method");
+}
+}
+class b extends a{
+ 
+// b()
+// {
+//     super.main();
+//      System.out.println(super.x);
+// }
+    //  void main(){
+    //     System.out.println("child ");
+    // }
+    void show(){
+        System.out.println("up casting");
+    }
+}
+public class Main {
+
+     public static void main(String[] args) {
+       
+        b ob=new b();
+        // b ob2 = (b) ob; 
+        ob.main();
+        // ob2`.show();
+
+     }
+}
